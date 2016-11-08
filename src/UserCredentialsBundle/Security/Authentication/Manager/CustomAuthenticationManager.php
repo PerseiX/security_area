3973a5e04fadbcce91f611af92b8c4bff289b92d
PerseiX <kamilrytel2@gmail.com>
2016-11-08 23:22:16 +0100

    Create CustomAuthenticationManager

---
+++ src/UserCredentialsBundle/Security/Authentication/Manager/CustomAuthenticationManager.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: persei
+ * Date: 11/8/16
+ * Time: 11:03 PM
+ */
+
+namespace UserCredentialsBundle\Security\Authentication\Manager;
+
+
+use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use UserCredentialsBundle\Security\Authentication\Provider\CustomAuthenticationProvider;
+
+class CustomAuthenticationManager implements AuthenticationManagerInterface
+{
+    /**
+     * @var CustomAuthenticationProvider
+     */
+    private $provider;
+
+    /**
+     * CustomAuthenticationManager constructor.
+     * @param CustomAuthenticationProvider $provider
+     */
+    public function __construct(CustomAuthenticationProvider $provider)
+    {
+        $this->provider = $provider;
+    }
+
+    /**
+     * @param TokenInterface $token
+     * @return \UserCredentialsBundle\Security\Authentication\Token\CustomToken
+     */
+    public function authenticate(TokenInterface $token)
+    {
+        if (!$this->provider->supports($token)) {
+            throw new AuthenticationException("The support of Token is invalid!");
+        }
+
+        $newToken = $this->provider->authenticate($token);
+        $newToken->setAuthenticated(true);
+
+        return $newToken;
+    }
+}
